e7edbfde126a0a5be60135749ae36897f0822c24
Eros Candelaresi <eros@candelaresi.de>
2017-09-23 18:36:32 +0200

    Changed build description to leverage Spring BOM files

--- src/main/java/de/erna/config/DatabaseSettings.java
+++ src/main/java/de/erna/config/DatabaseSettings.java
@@ -9,4 +9,20 @@ import org.springframework.stereotype.Component;
 public class DatabaseSettings {
     String host = "";
     String name = "";
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
